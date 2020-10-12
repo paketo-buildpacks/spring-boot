db10ec60c310e2f01de0bd7a991682d2c5f4fdb2
Ben Hale <bhale@vmware.com>
2020-10-12 06:44:39 -0700

    Compress Binaries
    This change updates the build scripts to compress the created binaries.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,12 @@
 
 set -euo pipefail
 
-if [[ -d ../go-cache ]]; then
-  GOPATH=$(realpath ../go-cache)
-  export GOPATH
-fi
-
 GOOS="linux" go build -ldflags='-s -w' -o bin/helper github.com/paketo-buildpacks/spring-boot/cmd/helper
 GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/paketo-buildpacks/spring-boot/cmd/main
+
+strip bin/helper bin/main
+upx -q -9 bin/helper bin/main
+
 ln -fs main bin/build
 ln -fs main bin/detect
 
