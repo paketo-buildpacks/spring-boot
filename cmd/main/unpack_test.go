b68968eaf14164590d9f29b4d624c78dd1c05734
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-02-14 15:53:30 -0500

    Spring Boot CDS * use Go zip to create both jars * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS * the feature is called CDS, not AppCDS

--- cmd/main/unpack_test.go
+++ cmd/main/unpack_test.go
@@ -41,6 +41,70 @@ func TestHelloName(t *testing.T) {
 
 }
 
+func TestOtherTest(t *testing.T) {
+
+	if err := zipSource("../../unpacked/content/META-INF", "run-app.jar"); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func zipSource(source, target string) error {
+	// 1. Create a ZIP file and zip.Writer
+	f, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := zip.NewWriter(f)
+	defer writer.Close()
+
+	// 2. Go through all the files of the source
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 3. Create a local file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// set compression
+		header.Method = zip.Store
+
+		// 4. Set relative path of a file as the header name
+		header.Name, err = filepath.Rel(filepath.Dir(source), path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		// 5. Create writer for the file header and save content of the file
+		headerWriter, err := writer.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(headerWriter, f)
+		return err
+	})
+}
+
 func writeRunAppJarManifest(originalJarExplodedDirectory string, runAppJarManifest *os.File, relocatedOriginalJar string) {
 	originalManifest, _ := libjvm.NewManifest(originalJarExplodedDirectory)
 	startClassValue, _ := retrieveStartClassValue(originalManifest)
