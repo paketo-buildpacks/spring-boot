f986f0d980aa9bf57d0e02c239f3e48a2c5f7f68
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-05-01 09:40:39 -0400

    try few things rename some others

--- cmd/main/unpack_test.go
+++ cmd/main/unpack_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"archive/zip"
+	"context"
 	"fmt"
 	"github.com/magiconair/properties"
+	"github.com/mholt/archiver/v4"
 	"github.com/paketo-buildpacks/libjvm"
 	"github.com/paketo-buildpacks/libpak/sherpa"
+	"github.com/saracen/fastzip"
 	"gopkg.in/yaml.v3"
 	"io"
 	"log"
@@ -47,6 +50,126 @@ func TestOtherTest(t *testing.T) {
 		log.Fatal(err)
 	}
 
+}
+
+func archiveWithFastZip(source, target string) {
+	// Create archiveWithFastZip file
+	w, err := os.Create(target)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	// Create new Archiver
+	//var options ArchiverOption = nil
+	a, err := fastzip.NewArchiver(w, source, fastzip.WithArchiverMethod(zip.Deflate))
+	if err != nil {
+		panic(err)
+	}
+	defer a.Close()
+
+	// Register a non-default level compressor if required
+	// a.RegisterCompressor(zip.Deflate, fastzip.FlateCompressor(1))
+
+	// Walk directory, adding the files we want to add
+	files := make(map[string]os.FileInfo)
+	err = filepath.Walk(source, func(pathname string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		files[pathname] = info
+		return nil
+	})
+
+	// Archive
+	if err = a.Archive(context.Background(), files); err != nil {
+		panic(err)
+	}
+}
+
+func archiveWithArchiver(source, target string) {
+
+	walkedFiles := make(map[string]string)
+	_ = filepath.Walk(source, func(pathname string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		walkedFiles[pathname], _ = filepath.Rel(filepath.Dir(source), pathname)
+		return nil
+	})
+
+	files, _ := archiver.FilesFromDisk(nil, walkedFiles)
+	out, _ := os.Create(target)
+	defer out.Close()
+
+	format := archiver.Zip{
+		SelectiveCompression: false,
+		Compression:          zip.Store,
+		ContinueOnError:      false,
+		TextEncoding:         "UTF8",
+	}
+	format.Archive(context.Background(), out, files)
+
+}
+
+func TestZip(t *testing.T) {
+	archiveWithArchiver("/Users/anthonyd2/workspaces/spring-cds-demo/build/libs/unzip/", "/Users/anthonyd2/workspaces/spring-cds-demo/build/libs/spring-cds-demo-1.0.0-SNAPSHOT-rezip.jar")
+}
+
+func zipSource2(source, target string) error {
+	// 1. Create a ZIP file and zip.Writer
+	f, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := zip.NewWriter(f)
+	defer writer.Close()
+
+	// 2. Go through all the files of the source
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 3. Create a local file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// set compression
+		header.Method = zip.Deflate
+
+		// 4. Set relative path of a file as the header name
+		header.Name, err = filepath.Rel(filepath.Dir(source), path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		// 5. Create writer for the file header and save content of the file
+		headerWriter, err := writer.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(headerWriter, f)
+		return err
+	})
 }
 
 func zipSource(source, target string) error {
--- go.mod
+++ go.mod
@@ -11,21 +11,39 @@ require (
 	github.com/paketo-buildpacks/libjvm v1.44.2
 	github.com/paketo-buildpacks/libpak v1.69.1
 	github.com/pelletier/go-toml v1.9.5
+	github.com/saracen/fastzip v0.1.11
 	github.com/sclevine/spec v1.4.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/bodgit/plumbing v1.2.0 // indirect
+	github.com/bodgit/sevenzip v1.3.0 // indirect
+	github.com/bodgit/windows v1.0.0 // indirect
+	github.com/connesc/cipherio v0.2.1 // indirect
 	github.com/creack/pty v1.1.21 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/h2non/filetype v1.1.3 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/mholt/archiver/v4 v4.0.0-alpha.8 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
 	github.com/pavlo-v-chernykh/keystore-go/v4 v4.5.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/saracen/zipextra v0.0.0-20220303013732-0187cb0159ea // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
